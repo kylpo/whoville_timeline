4f74048d447fbcdf4e298e8d3cce65293ad3677b
Kyle Poole <kyle@collectiveintellect.com>
2012-03-11 17:36:11 -0600

    connects to db and receives JSON. Ready to style/graph.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , routes = require('./routes');
-
 var app = module.exports = express.createServer();
+
+var ProfileDAO = require('./profileDAO').ProfileDAO;
+var profileDAO = new ProfileDAO('localhost', 27017);
 
 // Configuration
 
@@ -15,6 +15,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -29,7 +30,35 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+/**
+ * display all profiles in database
+ */
+app.get('/', function(req, res) {
+    profileDAO.findAll( function(error, profiles) {
+      console.log( profiles );
+        res.render('average_timeline.jade',
+        { locals: {
+          title: 'Average',
+          profiles: profiles
+        }
+        });
+    });
+});
+
+/**
+ * display all profiles, of user :id, in database
+ */
+app.get('/:id', function(req, res) {
+    profileDAO.findById(req.params.id, function(error, profiles) {
+      console.log( profiles );
+        res.render('timeline.jade',
+        { locals: {
+          title: req.params.id,
+          profiles: profiles
+        }
+        });
+    });
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ profileDAO.js
@@ -0,0 +1,48 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+
+ProfileDAO = function(host, port) {
+  this.db = new Db('test', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+ProfileDAO.prototype.getCollection = function(callback) {
+  this.db.collection('whoville_timeline', function(error, profile_collection) {
+    if (error) callback(error);
+    else callback(null, profile_collection);
+  });
+};
+
+/**
+ * returns array of ALL profiles in JSON form
+ */
+ProfileDAO.prototype.findAll = function(callback) {
+    this.getCollection(function(error, profile_collection) {
+      if (error) callback(error);
+      else {
+        profile_collection.find().toArray(function(error, results) {
+          if (error) callback(error);
+          else callback(null, results);
+        });
+      }
+    });
+};
+
+/**
+ * returns array of user profiles in JSON form
+ * requires valid id
+ */
+ProfileDAO.prototype.findById = function(id, callback) {
+    this.getCollection(function(error, profile_collection) {
+      if (error) callback(error);
+      else {
+        profile_collection.find({id: id}).toArray(function(error, result) {
+          if (error) callback(error);
+          else callback(null, result);
+        });
+      }
+    });
+};
+
+exports.ProfileDAO = ProfileDAO;
